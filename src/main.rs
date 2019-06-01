35b757388b2b67d8f27c52bc755b0a649df7c336
Seán Kelly <daxorinator@gmail.com>
2019-06-01 15:00:13 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 #![no_std]
-#![feature(lang_items, start)]
+#![feature(start)]
+#![no_main]
+
+#[no_mangle]
+fn main() {
+	0;
+}
 
 use core::panic::PanicInfo;
 
+#[no_mangle]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
-}
-
-#[link(name = "MLlib", kind = "static")]
-extern "C" {
-    fn ML_Init();
-    fn ML_SplashScreen();
-}
-
-#[start]
-fn start(_argc: isize, _argv: *const *const u8) -> isize {
-	unsafe {
-		ML_Init();
-		ML_SplashScreen();
-	}
-	0
 }
