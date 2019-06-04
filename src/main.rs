14613d541c1ed14faa48fa56f6262c161178e1b8
Seán Kelly <daxorinator@gmail.com>
2019-06-04 23:52:44 +0100

    Updated to MLlib example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 #![no_std]
-#![feature(start)]
+#![feature(rustc_private)]
 #![no_main]
 
+extern crate mllib_sys;
+
 use core::panic::PanicInfo;
+use mllib_sys::{ML_Init, ML_SplashScreen, ML_Refresh};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[start]
 #[no_mangle]
-pub extern "C" fn _start(_argc: i32, _argv: *const *const u8) -> i32 {
-    0
+pub extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
+	unsafe{
+		ML_Init();
+		ML_SplashScreen();
+		
+		loop {
+			ML_Refresh();
+		}
+	}
+	
+	0	
+
 }
