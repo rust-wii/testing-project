5273d025fd7b19fe9e3576d03033d404e3c8b6f4
Seán Kelly <daxorinator@gmail.com>
2019-06-07 21:53:22 +0100

    Readded build.rs yay it works

---
+++ build.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let dkp_path = std::env::var("DEVKITPRO").unwrap();
+    println!("cargo:rustc-link-search=native={}/MLlib/lib", dkp_path);
+    println!("cargo:rustc-link-lib=static=MLlib");
+    println!("cargo:rustc-link-search=native={}/libogc/lib/wii", dkp_path);
+    println!("cargo:rustc-link-search=native={}/devkitPPC/powerpc-eabi/lib", dkp_path);
+    println!("cargo:rustc-link-lib=static=wiiuse");
+    println!("cargo:rustc-link-lib=static=ogc");
+    println!("cargo:rustc-link-lib=static=png");
+    println!("cargo:rustc-link-lib=static=bte");
+    println!("cargo:rustc-link-lib=static=sysbase");
+    println!("cargo:rustc-link-lib=static=z");
+    println!("cargo:rustc-link-lib=static=freetype");
+    println!("cargo:rustc-link-lib=static=m");
+    println!("cargo:rustc-link-lib=static=asnd");
+}
